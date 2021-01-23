715efe259f32ae36e5aba4eb6d6ed5f7c9f761a9
yamaguchi tsuyoshi <yamaguchi11057@gmail.com>
2021-01-23 22:41:31 +0900

    2nd_commit

--- psql_test/settings_common.py
+++ psql_test/settings_common.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 
@@ -122,3 +123,10 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'alert alert-danger',
+    messages.WARNING: 'alert alert-warning',
+    messages.SUCCESS: 'alert alert-success',
+    messages.INFO: 'alert alert-info',
+}
--- tobuyapp/views.py
+++ tobuyapp/views.py
@@ -1,4 +1,5 @@
 import logging
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
 from .forms import InquiryForm
@@ -13,8 +14,10 @@ class InquiryView(FormView):
     template_name = 'inquiry.html'
     form_class = InquiryForm
     success_url = reverse_lazy('tobuyapp:inquiry')
-    
+
     def form_valid(self, form):
         form.send_email()
+        messages.success(self.request, 'The message has been sent')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
+        
