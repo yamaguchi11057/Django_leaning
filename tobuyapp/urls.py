49342a5e00619b3edd8a86b16e7553b93cd801af
yamaguchi tsuyoshi <yamaguchi11057@gmail.com>
2021-02-01 22:59:17 +0900

    commit-210201

--- tobuyapp/urls.py
+++ tobuyapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from .views import ToBuyList, InquiryView, DiaryListView
+from .views import ToBuyList, InquiryView, DiaryListView, DiaryDetailView, DiaryCreateView
 
 app_name="tobuyapp"
 urlpatterns = [
     path('', ToBuyList.as_view(), name="index"),
     path('inquiry/', InquiryView.as_view(), name="inquiry"),
     path('diary_list/', DiaryListView.as_view(), name="diary_list"),
+    path('diary_detail/<int:pk>/', DiaryDetailView.as_view(), name="diary_detail"),
+    path('diary_create/', DiaryCreateView.as_view(), name="diary_create"),
     
 ]
--- tobuyapp/views.py
+++ tobuyapp/views.py
@@ -1,9 +1,9 @@
 import logging
 from django.contrib import messages
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, FormView, ListView
+from django.views.generic import TemplateView, FormView, ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import InquiryForm
+from .forms import InquiryForm, DiaryCreateForm
 from .models import Diary
 
 logger = logging.getLogger(__name__)
@@ -26,7 +26,28 @@ class InquiryView(FormView):
 class DiaryListView(LoginRequiredMixin, ListView):
     template_name = 'diary_list.html'
     model =Diary
+    paginate_by = 2
     def get_queryset(self):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return diaries
         
+class DiaryDetailView(DetailView):
+    model = Diary
+    template_name = 'diary_detail.html'
+
+class DiaryCreateView(CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('tobuyapp:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request, '日記を作成しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, '日記の作成に失敗しました。')
+        return super().form_invalid(form)
